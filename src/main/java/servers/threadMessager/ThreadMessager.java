ea1749972a62d2d94f2173c7e7933e3d56fab1bc
Roland4444 <turbopollux@rambler.ru>
2020-12-15 18:00:16 +0400

    lambda potok

--- src/main/java/servers/threadMessager/ThreadMessager.java
+++ src/main/java/servers/threadMessager/ThreadMessager.java
@@ -1,11 +1,6 @@
 package servers.threadMessager;
-
-import io.javalin.Javalin;
-
-import java.net.UnknownHostException;
-
 public class ThreadMessager extends Thread{
-    
+
     public void run() {
         while(true) {
             try {
