9ceb7a32f582a33751a7c79246f1fc7354aa74a5
Roland4444 <turbopollux@rambler.ru>
2021-01-11 17:59:12 +0400

    update

---
+++ nextjs-app/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    return { userAgent }
+  }
+  render() {
+    return (
+      <div>
+        Hello World {this.props.userAgent}
+      </div>
+    )
+  }
+}
+
